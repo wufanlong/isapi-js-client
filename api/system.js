af94530096c81a1a546d56d16940f528c4c54928
wufanlong <14043878+wufanlong@user.noreply.gitee.com>
2026-01-09 18:25:32 +0800

    新增网络接口获取网关子网掩码

--- api/system.js
+++ api/system.js
@@ -1,6 +1,11 @@
 import request from '../utils/request.js';
 
 const system = {
+    network: {
+        interfaces(options) {
+            return request.get(`http://${options.ip}/ISAPI/System/Network/interfaces`, options.axiosOptions);
+        }
+    },
     deviceInfo(options) {
         return request.get(`http://${options.ip}/ISAPI/System/deviceInfo`, options.axiosOptions);
     }
