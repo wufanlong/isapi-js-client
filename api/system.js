3c521171c1118e7e5c1e7c1e1ab00c7897116179
wufanlong <14043878+wufanlong@user.noreply.gitee.com>
2026-01-22 16:47:53 +0800

    新增接口

--- api/system.js
+++ api/system.js
@@ -8,7 +8,25 @@ const system = {
     },
     getDeviceInfo(options) {
         return request.get(`http://${options.ip}/ISAPI/System/deviceInfo`, options.axiosOptions);
-    }
+    },
+    getVideoInputsChannelsOverlaysByID(options) {
+        return request.get(`http://${options.ip}/ISAPI/System/Video/inputs/channels/${options.axiosPathVal[0]}/overlays`, options.axiosOptions)
+    },
+    putVideoInputsChannelsOverlaysByID(options) {
+        return request.put(`http://${options.ip}/ISAPI/System/Video/inputs/channels/${options.axiosPathVal[0]}/overlays`, options.axiosData, options.axiosOptions)
+    },
+    getVideoInputsChannelsOverlaysChannelNameOverlayByID(options) {
+        return request.get(`http://${options.ip}/ISAPI/System/Video/inputs/channels/${options.axiosPathVal[0]}/overlays/channelNameOverlay`, options.axiosOptions)
+    },
+    putVideoInputsChannelsOverlaysChannelNameOverlayByID(options) {
+        return request.put(`http://${options.ip}/ISAPI/System/Video/inputs/channels/${options.axiosPathVal[0]}/overlays/channelNameOverlay`, options.axiosData, options.axiosOptions)
+    },
+    getVideoInputsChannelsByID(options) {
+        return request.get(`http://${options.ip}/ISAPI/System/Video/inputs/channels/${options.axiosPathVal[0]}`, options.axiosOptions)
+    },
+    putVideoInputsChannelsByID(options) {
+        return request.put(`http://${options.ip}/ISAPI/System/Video/inputs/channels/${options.axiosPathVal[0]}`, options.axiosData, options.axiosOptions)
+    },
 }
 
 export default system;
